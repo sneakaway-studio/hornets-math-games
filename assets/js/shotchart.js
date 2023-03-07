df1cb475a311521e4425912a6c91fc7164e4d803
Owen Mundy <omundy@gmail.com>
2023-03-07 10:13:21 -0500

    Adds new menu (html only), format (only) D3

--- assets/js/shotchart.js
+++ assets/js/shotchart.js
@@ -1,23 +1,27 @@
 "use strict";
 
-
-
 // Add point types here
 const pointTypes = ["3 pts", "2 pts", "0 (missed 3)", "0 (missed 2)"];
 let pointType = pointTypes[0];
 
 // Add shot types here
-const shotTypes = ["layup/dunk", "dribble jumper", "catch & shoot", "runner/floater", "post move", "remove"];
+const shotTypes = [
+	"layup/dunk",
+	"dribble jumper",
+	"catch & shoot",
+	"runner/floater",
+	"post move",
+	"remove",
+];
 let shotType = shotTypes[0];
 
 // Set up color and shape
-const eventColor = d3.scaleOrdinal()
-    .domain(pointTypes)
-    .range(d3.schemeCategory10);
+const eventColor = d3
+	.scaleOrdinal()
+	.domain(pointTypes)
+	.range(d3.schemeCategory10);
 
-const eventShape = d3.scaleOrdinal()
-    .domain(shotTypes)
-    .range(d3.symbols);
+const eventShape = d3.scaleOrdinal().domain(shotTypes).range(d3.symbols);
 
 const shapeSize = 10;
 
@@ -25,193 +29,207 @@ createControls();
 createClickMap();
 
 function createControls() {
+	// Radio buttons for points
+	const pointEnter = d3
+		.select("#pointTypeButtons")
+		.selectAll(".form-check")
+		.data(pointTypes)
+		.enter()
+		.append("div")
+		.attr("class", "form-check pl-4 mb-1  pointTypeRadioWrapper")
+		.style("background-color", (d) => {
+			const c = d3.color(eventColor(d));
+			c.opacity = 0.75;
+			return c;
+		})
+		.style("border", "1px solid rgba(0, 0, 0, 0.75")
+		.style("border-radius", "5px")
+		.on("click", (d) => (pointType = d));
 
-  // Radio buttons for points
-  const pointEnter = d3.select("#pointTypeButtons").selectAll(".form-check")
-      .data(pointTypes)
-    .enter().append("div")
-      .attr("class", "form-check pl-4 mb-1  pointTypeRadioWrapper")
-      .style("background-color", d => {
-        const c = d3.color(eventColor(d));
-        c.opacity = 0.75;
-        return c;
-      })
-      .style("border", "1px solid rgba(0, 0, 0, 0.75")
-      .style("border-radius", "5px")
-      .on("click", d => pointType = d);
+	pointEnter
+		.append("input")
+		.attr("type", "radio")
+		.attr("class", "form-check-input")
+		.attr("name", "pointTypeRadio")
+		.attr("id", (d) => d + "_radio")
+		.attr("value", (d) => d)
+		.property("defaultChecked", (d) => d === pointType);
 
-  pointEnter.append("input")
-      .attr("type", "radio")
-      .attr("class", "form-check-input")
-      .attr("name", "pointTypeRadio")
-      .attr("id", d => d + "_radio")
-      .attr("value", d => d)
-      .property("defaultChecked", d => d === pointType);
+	pointEnter
+		.append("label")
+		.text((d) => d)
+		.attr("class", "form-check-label")
+		.attr("for", (d) => d + "_radio");
 
-  pointEnter.append("label")
-      .text(d => d)
-      .attr("class", "form-check-label")
-      .attr("for", d => d + "_radio");
+	// Radio buttons for shot types
+	const shotEnter = d3
+		.select("#shotTypeButtons")
+		.selectAll(".form-check")
+		.data(shotTypes)
+		.enter()
+		.append("div")
+		.attr("class", "form-check shotTypeRadioWrapper")
+		.on("click", (d) => (shotType = d));
 
-  // Radio buttons for shot types
-  const shotEnter = d3.select("#shotTypeButtons").selectAll(".form-check")
-      .data(shotTypes)
-    .enter().append("div")
-      .attr("class", "form-check shotTypeRadioWrapper")
-      .on("click", d => shotType = d);
+	shotEnter
+		.append("input")
+		.attr("type", "radio")
+		.attr("class", "form-check-input")
+		.attr("name", "shotTypeRadio")
+		.attr("id", (d) => d + "_radio")
+		.attr("value", (d) => d)
+		.property("defaultChecked", (d) => d === shotType);
 
-  shotEnter.append("input")
-      .attr("type", "radio")
-      .attr("class", "form-check-input")
-      .attr("name", "shotTypeRadio")
-      .attr("id", d => d + "_radio")
-      .attr("value", d => d)
-      .property("defaultChecked", d => d === shotType);
+	shotEnter
+		.append("label")
+		.text((d) => d)
+		.attr("class", "form-check-label mr-1")
+		.attr("for", (d) => d + "_radio")
+		.style("vertical-align", "20%");
 
-  shotEnter.append("label")
-      .text(d => d)
-      .attr("class", "form-check-label mr-1")
-      .attr("for", d => d + "_radio")
-      .style("vertical-align", "20%");
+	const w = shapeSize * 2;
+	const h = shapeSize * 2;
 
-  const w = shapeSize * 2;
-  const h = shapeSize * 2;
+	shotEnter
+		.filter((d) => d !== "remove")
+		.append("svg")
+		.attr("width", w)
+		.attr("height", h)
+		.append("g")
+		.attr("class", "event")
+		.attr("transform", (d) => "translate(" + w / 2 + "," + h / 2 + ")")
+		.append("path")
+		.attr("d", (d) => getShape(d)());
 
-  shotEnter.filter(d => d !== "remove").append("svg")
-      .attr("width", w)
-      .attr("height", h)
-    .append("g")
-      .attr("class", "event")
-      .attr("transform", d => "translate(" + (w / 2) + "," + (h / 2) + ")")
-    .append("path")
-      .attr("d", d => getShape(d)());
+	// Download button
+	d3.select("#downloadButton").on("click", () => {
+		let data = [];
+		let columns = [];
 
-  // Download button
-  d3.select("#downloadButton").on("click", () => {
-    let data = [];
-    let columns = [];
+		// Get event data
+		d3.select("#clickMap")
+			.selectAll(".event")
+			.each((d, i) => {
+				if (i === 0) {
+					// Get columns from first event
+					columns = d3.keys(d);
+					data = columns.join(",") + "\n";
+				}
 
-    // Get event data
-    d3.select("#clickMap").selectAll(".event").each((d, i) => {
-      if (i === 0) {
-        // Get columns from first event
-        columns = d3.keys(d);
-        data = columns.join(",") + "\n";
-      }
+				// Add event data
+				data += columns.map((column) => d[column]).join(",") + "\n";
+			});
 
-      // Add event data
-      data += columns.map(column => d[column]).join(",") + "\n";
-    });
+		// Create temporary download link for data
+		const a = window.document.createElement("a");
+		a.href = window.URL.createObjectURL(
+			new Blob([data], { type: "text/csv" })
+		);
+		a.download = d3.select("#filenameInput").property("value");
 
-    // Create temporary download link for data
-    const a = window.document.createElement("a");
-    a.href = window.URL.createObjectURL(new Blob([data], {type: "text/csv"}));
-    a.download = d3.select("#filenameInput").property("value");
+		// Append anchor to body
+		document.body.appendChild(a);
+		a.click();
 
-    // Append anchor to body
-    document.body.appendChild(a)
-    a.click();
+		// Remove anchor from body
+		document.body.removeChild(a);
+	});
 
-    // Remove anchor from body
-    document.body.removeChild(a)
-  });
-
-  // Clear button
-  d3.select("#clearButton").on("click", () => {
-    d3.select("#clickMap").selectAll(".event").remove();
-  });
+	// Clear button
+	d3.select("#clearButton").on("click", () => {
+		d3.select("#clickMap").selectAll(".event").remove();
+	});
 }
 
 function createClickMap() {
-  d3.select("#clickMap").select("img").on("click", () => {
-    d3.event.preventDefault();
-  });
+	d3.select("#clickMap")
+		.select("img")
+		.on("click", () => {
+			d3.event.preventDefault();
+		});
 
-  // Select the svg element
-  const svg = d3.select("#clickMap").select("svg");
+	// Select the svg element
+	const svg = d3.select("#clickMap").select("svg");
 
+	// Receive click events from background
+	svg.on("click", function () {
+		if (shotType === "remove") return;
 
+		// Get the width and height
+		const width = parseInt(svg.style("width"), 10);
+		const height = parseInt(svg.style("height"), 10);
 
-  // Receive click events from background
-  svg.on("click", function() {
+		const x = d3.mouse(this)[0];
+		const y = d3.mouse(this)[1];
 
-    if (shotType === "remove") return;
+		// Set the data for this event
+		const data = {
+			x: x,
+			y: y,
+			xNorm: x / width,
+			yNorm: y / height,
+			pointType: pointType,
+			shotType: shotType,
+		};
 
-    // Get the width and height
-    const width = parseInt(svg.style("width"), 10);
-    const height = parseInt(svg.style("height"), 10);
+		// Draw the event
+		const g = svg
+			.append("g")
+			.datum(data)
+			.attr("class", "event")
+			.attr("transform", (d) => "translate(" + d.x + "," + d.y + ")")
+			.on("click", function () {
+				if (shotType === "remove") {
+					d3.event.stopPropagation();
 
-    const x = d3.mouse(this)[0];
-    const y = d3.mouse(this)[1];
+					d3.select(this).remove();
 
-    // Set the data for this event
-    const data = {
-      x: x,
-      y: y,
-      xNorm: x / width,
-      yNorm: y / height,
-      pointType: pointType,
-      shotType: shotType
-    };
+					return;
+				}
+			});
 
-    // Draw the event
-    const g = svg.append("g").datum(data)
-        .attr("class", "event")
-        .attr("transform", d => "translate(" + d.x + "," + d.y + ")")
-        .on("click", function() {
-          if (shotType === "remove") {
-            d3.event.stopPropagation();
+		g.append("path")
+			.attr("d", (d) => getShape(d.shotType)())
+			.style("fill", (d) => eventColor(d.pointType));
+	});
 
-            d3.select(this).remove();
+	// Handle resizing svg div
+	const resizeObserver = new ResizeObserver((entries) => {
+		// Get the width and height
+		const width = parseInt(svg.style("width"), 10);
+		const height = parseInt(svg.style("height"), 10);
 
-            return;
-          }
-        });
+		d3.select("#clickMap")
+			.selectAll(".event")
+			.each(function (d) {
+				d.x = d.xNorm * width;
+				d.y = d.yNorm * height;
 
-    g.append("path")
-        .attr("d", d => getShape(d.shotType)())
-        .style("fill", d => eventColor(d.pointType));
+				d3.select(this).attr(
+					"transform",
+					(d) => "translate(" + d.x + "," + d.y + ")"
+				);
+			});
+	});
 
-  });
-
-    // Handle resizing svg div
-    const resizeObserver = new ResizeObserver(entries => {
-      // Get the width and height
-      const width = parseInt(svg.style("width"), 10);
-      const height = parseInt(svg.style("height"), 10);
-  
-      d3.select("#clickMap").selectAll(".event").each(function (d) {
-        d.x = d.xNorm * width;
-        d.y = d.yNorm * height;
-  
-        d3.select(this).attr("transform", d => "translate(" + d.x + "," + d.y + ")")
-      });
-    });
-  
-    resizeObserver.observe(document.querySelector(".svgDiv"));
+	resizeObserver.observe(document.querySelector(".svgDiv"));
 }
-
-
 
 function getShape(shotType) {
-  return d3.symbol()
-    .type(eventShape(shotType))
-    .size(shapeSize * shapeSize);
+	return d3
+		.symbol()
+		.type(eventShape(shotType))
+		.size(shapeSize * shapeSize);
 }
-
 
 // let shotChartSummary = {};
 // let shotChartSummaryStart = {
 // 	tossCount: 0,
 
-
 // };
 // let nodes = {
 // 	tossBtn: {},
 // 	resetBtn: {},
-
-
-
 
 // };
 // nodes = getNodes(nodes);
@@ -258,7 +276,7 @@ function getShape(shotType) {
 // 		"tossEventsEstimateAttendance",
 // 		Math.max(1, (shotChartSummary.gameAttendance / shotChartSummary.tossCount).toFixed(2))
 // 	);
-	
+
 // 	nodes.tossCountLg.value = shotChartSummary.tossCount || 0;
 // 	// nodes.tossCount.innerHTML = shotChartSummary.tossCount || 0;
 // 	nodes.tossEventsEstimate1000.innerHTML =
